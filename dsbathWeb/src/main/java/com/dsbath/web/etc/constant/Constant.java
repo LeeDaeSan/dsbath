11418c24379bc733f03efe9ec82617003748868f
이대산 <idaesan@idaesan-ui-MacBookPro.local>
2020-11-09 18:05:24 +0900

    web config set

---
+++ dsbathWeb/src/main/java/com/dsbath/web/etc/constant/Constant.java
@@ -0,0 +1,16 @@
+package com.dsbath.web.etc.constant;
+
+public class Constant {
+
+	//view default path
+	public static final String VIEWS 	= "views";
+	public static final String NOTILES 	= "notiles";
+	
+	public static final String INDEX_PAGE 	= "dashboard";
+	public static final String LOGIN_PAGE 	= "login";
+	public static final String LOGOUT_PAGE 	= "logout";
+	
+	public static final String MERGE_TYPE_INSERT = "I"; 	// 등록
+	public static final String MERGE_TYPE_UPDATE = "U";		// 수정
+	public static final String MERGE_TYPE_DELETE = "D";		// 삭제
+}
